fad236503fb2564c0f89e9a6a7b455f205d61295
Myroslav Velychko <myroslav@janeladigital.com>
2024-05-20 17:25:19 +0100

    Add

--- CRMScrap/Form1.cs
+++ CRMScrap/Form1.cs
@@ -7,7 +7,9 @@ namespace CRMScrap
     {
         private ChromeDriver _driver;
         private Thread _thread;
-
+        //private WebScrap _scrap;
+        //private bool _isLoggedIn = false;
+        
         public Form1()
         {
             InitializeComponent();
@@ -26,7 +28,6 @@ namespace CRMScrap
                 Login_BTN.ForeColor = Color.Red;
                 Login_BTN.Cursor = Cursors.No;
             }
-
         }
 
         private void LoginButton(object sender, EventArgs e)
@@ -43,38 +44,65 @@ namespace CRMScrap
             Login_BTN.ForeColor = Color.Gold;
             Login_BTN.UseWaitCursor = true;
             Login_BTN.Text = "Testing...";
-            OpenSelenium();
-            Thread.Sleep(3000);
-            Login(Username_TextBox.Text, Password_TextBox.Text);
+            OpenSelenium(); Thread.Sleep(3000);
+            Login(Username_TextBox.Text, Password_TextBox.Text); Thread.Sleep(100);
+            ScrapInfoOnImovelTable();
             //CloseSelenium();
             Login_BTN.ForeColor = Color.Lime;
             Login_BTN.UseWaitCursor = false;
             Login_BTN.Text = "Login";
-
         }
-
-        private void Login(string username, string password)
-        {
-
-            try
-            {
-                _driver.FindElements(By.XPath("//*[@id=\"username\"]"))[0].SendKeys(username); Thread.Sleep(3000);
-                _driver.FindElements(By.XPath("/html/body/div[2]/div/div/div/div[1]/div[2]/div/form/div[2]/input"))[0].SendKeys(password); Thread.Sleep(2000);
-                _driver.FindElement(By.XPath("//*[@id=\"login\"]/div[3]/button")).Click(); Thread.Sleep(1000);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-        }
-
+        
         private void OpenSelenium()
         {
             ChromeDriverService service = ChromeDriverService.CreateDefaultService();
             service.HideCommandPromptWindow = true;
             _driver = new ChromeDriver(service);
             _driver.Navigate().GoToUrl("https://app.imo360crm.pt/login");
+        }
 
+        private void Login(string username, string password)
+        {
+            try
+            {
+                _driver.FindElements(By.XPath("//*[@id=\"username\"]"))[0].SendKeys(username); Thread.Sleep(3000);
+                _driver.FindElements(By.XPath("/html/body/div[2]/div/div/div/div[1]/div[2]/div/form/div[2]/input"))[0].SendKeys(password); Thread.Sleep(2000);
+                _driver.FindElement(By.XPath("//*[@id=\"login\"]/div[3]/button")).Click(); Thread.Sleep(1000);
+                
+                // if (_driver.FindElements(By.XPath("//element_after_login")).Count > 0)
+                // {
+                //     _isLoggedIn = true;
+                // }   
+                
+                _driver.Navigate().GoToUrl("https://app.imo360crm.pt/listagem/imoveis"); Thread.Sleep(1000);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        
+        private void ScrapInfoOnImovelTable()
+        { 
+            var items = AllParameters();
+            //_driver.Navigate().GoToUrl("https://app.imo360crm.pt/listagem/imoveis"); Thread.Sleep(1000);        
+            // Find elements that contain the product details
+            IReadOnlyCollection<IWebElement> productElements = _driver.FindElements(By.XPath("//*[@id=\"customtable\"]"));
+
+            // Loop through the product elements and extract the desired information
+            foreach (IWebElement productElement in productElements)
+            {
+                string url = productElement.FindElement(By.XPath("//*[@id=\"tech-companies-1-clone\"]/tbody/tr[1]/td[1]")).Text;
+                string reference = productElement.FindElement(By.XPath("//*[@id=\"tech-companies-1-clone\"]/tbody/tr[1]/td[1]")).Text;
+
+                // Add the item details to the list
+                items.Add(new string[] { url, reference });
+            }
+        }
+    
+        private static List<string[]> AllParameters()
+        {
+            return new List<string[]>();
         }
 
         private void CloseForm(object sender, FormClosedEventArgs e)
--- CRMScrap/WebScrap.cs
+++ CRMScrap/WebScrap.cs
@@ -1,6 +1,38 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
 namespace CRMScrap;
 
-public class WebScrap
+/*public class WebScrap
 {
+    private IWebDriver _driver = new ChromeDriver();
+    private bool _isLoggedIn;
+
+    internal void ScrapInfoOnImovelTable()
+    { 
+        if (!_isLoggedIn)
+        {
+            throw new InvalidOperationException("You must be logged in to scrape information.");
+        }
+        
+        var items = AllParameters();
+        _driver.Navigate().GoToUrl("https://app.imo360crm.pt/listagem/imoveis"); Thread.Sleep(1000);        
+        // Find elements that contain the product details
+        IReadOnlyCollection<IWebElement> productElements = _driver.FindElements(By.XPath("//*[@id=\"customtable\"]"));
+
+        // Loop through the product elements and extract the desired information
+        foreach (IWebElement productElement in productElements)
+        {
+            string url = productElement.FindElement(By.XPath("//*[@id=\"tech-companies-1-clone\"]/tbody/tr[1]/td[1]")).Text;
+            string reference = productElement.FindElement(By.XPath("//*[@id=\"tech-companies-1-clone\"]/tbody/tr[1]/td[1]")).Text;
+
+            // Add the item details to the list
+            items.Add(new string[] { url, reference });
+        }
+    }
     
-}
+    private static List<string[]> AllParameters()
+    {
+        return new List<string[]>();
+    }
+}*/
